2fc1fe9dd89b18981870d76ba4d111eb8448e46f
Hubert Krzysik <h.krzysik@avanade.com>
2022-04-04 16:46:21 +0200

    typescript

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Navbar from "../navbar";
 import { Box, Container } from "@chakra-ui/react";
 
 type MainProps = {
@@ -13,6 +14,7 @@ const Main: React.FC<MainProps> = ({ children, router }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Mariusz Cyags - Homepage</title>
       </Head>
+      <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
         {children}
       </Container>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react";
+
 const Page: React.FC = () => {
-  return <div>Hello</div>;
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} alignContent="center">
+        Hello, I&apos;m a front-end developer based in Poland!
+      </Box>
+      <Box display={{ md: "flex" }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Mariusz Cygas
+          </Heading>
+          <p>Digital Craftzman ( Artist / Developer / Designer / Skater )</p>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Page;
